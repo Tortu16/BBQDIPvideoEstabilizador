4ac1ed7ffcf41d4cc046a4c5995d49f17432ff78
Tortu16 <luisdjm@gmail.com>
2016-07-10 10:34:20 -0600

    Update GeneracionVideo.m

--- GeneracionVideo.m
+++ GeneracionVideo.m
@@ -1,44 +1,81 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%  Construcci?n del v?deo inestable
+%  Construcción del vídeo inestable
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+clear
+
 % Imagen de referencia
 filename = 'pout.tif';
-A = imread(filename);
+inputImage = imread(filename);
 info = imfinfo(filename);
 
-% Definici?n de variables aleatorias de desestabilizaci?n
+% Definición de variables aleatorias de desestabilización
 theta = 0;
+phi = 0;
 tx = 0;
 ty = 0;
-m = [cosd(theta) -sind(theta) 0; 
-     sind(theta)  cosd(theta) 0; 
-     tx           ty          1];
+R_theta = [cosd(theta) -sind(theta);
+           sind(theta)  cosd(theta)];
+R_phi =   [cosd(phi) -sind(phi);
+           sind(phi)  cosd(phi)]; 
+R_mphi =   [cosd(-phi) -sind(-phi);
+           sind(-phi)  cosd(-phi)]; 
+lambda1 = 0;
+lambda2 = 0;
+D = [lambda1 0;0 lambda2];
+A = R_theta*R_mphi*D*R_phi;
+m = [A(1,1) A(1,2) tx; 
+     A(2,1) A(2,2) ty; 
+     0      0      1];
 
- % Definici?n de las caracter?sticas estad?sticas del ruido
+ % Definición de las características estadísticas del ruido
  
  var_theta = 1;
  var_tx = 1;
  var_ty = 1;
  
- % Construcci?n del v?deo cuadro a cuadro
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ %
+ %       Construcción del vídeo 
+ %
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  
- for n = 1:150
+ vid_Duration = 5; %5s
+ frames = vid_Duration*30; % 30 fps.
+ 
+ % Inicialización de la pila de imágenes que se convierten a vídeo
+ RGB = cat (3, inputImage, inputImage ,inputImage);
+ f(frames) = im2frame(RGB);
+ 
+ % La imágen se distorciona aleatoriamente en cada cuadro
+ % El vídeo se compone de la secuencia de imágenes deformadas
+ 
+ for n = 1:frames
     theta = var_theta * randn;
     tx = var_tx * randn;
     ty = var_ty * randn;
-    m = [cosd(theta) -sind(theta) 0; 
-         sind(theta)  cosd(theta) 0; 
-         tx           ty          1];
+    R_theta = [cosd(theta) -sind(theta);
+               sind(theta)  cosd(theta)];
+    R_phi =   [cosd(phi) -sind(phi);
+               sind(phi)  cosd(phi)]; 
+    R_mphi =   [cosd(-phi) -sind(-phi);
+               sind(-phi)  cosd(-phi)]; 
+    lambda1 = 0.01*randn+1;
+	lambda2 = 0.01*randn+1;
+    D = [lambda1 0;0 lambda2];
+    A = R_theta*R_mphi*D*R_phi;
+    m = [A(1,1) A(2,1) 0; 
+         A(1,2) A(2,2) 0; 
+         tx      ty    1];
     tform = affine2d(m);
-    outputImage = imwarp(A,tform);
+    outputImage = imwarp(inputImage,tform);
     RGB = cat (3, outputImage, outputImage ,outputImage);
     f(n) = im2frame (RGB);
  end
  
- % Reproducci?n del v?deo
+ % Reproducción del vídeo
  movie(f)
 
 
